1f4ed6b3800a724781735278c04647da49510092
vijay21500269 <94381788+vijay21500269@users.noreply.github.com>
2023-04-05 19:27:54 +0530

    Add files via upload

---
+++ exp4 Dip.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import matplotlib.pyplot as plt
+gray_image = cv2.imread("cat.jpg")
+color_image = cv2.imread("house.jpeg",-1)
+cv2.imshow("Gray Image",gray_image)
+cv2.imshow("Colour Image",color_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# In[2]:
+
+
+gray_hist = cv2.calcHist([gray_image],[0],None,[256],[0,256])
+color_hist = cv2.calcHist([color_image],[0],None,[256],[0,256])
+plt.figure()
+plt.imshow(gray_image)
+plt.show()
+plt.title("Histogram")
+plt.xlabel("Grayscale Value")
+plt.ylabel("Pixel Count")
+plt.stem(gray_hist)
+plt.show()
+plt.imshow(color_image)
+plt.show()
+plt.title("Histogram of Color Image - Green Channel")
+plt.xlabel("Intensity Value")
+plt.ylabel("Pixel Count")
+plt.stem(color_hist)
+plt.show()
+
+
+# In[4]:
+
+
+gray_image = cv2.imread("cat.jpg",0)
+cv2.imshow("Gray Image",gray_image)
+equ = cv2.equalizeHist(gray_image)
+cv2.imshow("Equalized Image",equ)
+cv2.waitKey(0)
+cv2.destroyAllWindows
+
+
+# In[ ]:
+
+
+
+
